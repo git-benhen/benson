f52c8f77e4f77755e28c7a8bf4de99553a0905b7
BENSON <BENSON@LAPTOP-N29UTT3F>
2022-01-28 10:05:01 +0530

    second commit

--- java project/src/ben.java
+++ java project/src/ben.java
@@ -1,10 +1,6 @@
 // yes i got it
 public class ben {
-public static void main(String[] args) {
-	System.out.println("hellojava");
-}
-
-public static void main(String[] args) {
-	System.out.println("hellojava");
+ public static void main(String[] args) {
+	System.out.println("hello ben");
 }
 }
