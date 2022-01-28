dfe97df6f48f765d4b743e65640c72c1f5953efa
BENSON <BENSON@LAPTOP-N29UTT3F>
2022-01-28 09:11:40 +0530

    fisrt commit

--- java project/src/ben.java
+++ java project/src/ben.java
@@ -3,4 +3,8 @@ public class ben {
 public static void main(String[] args) {
 	System.out.println("hellojava");
 }
+
+public static void main(String[] args) {
+	System.out.println("hellojava");
+}
 }
