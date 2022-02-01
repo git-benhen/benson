9e628e8deb1bd9dc5468fa636c42263ba75302bb
BENSON <BENSON@LAPTOP-N29UTT3F>
2022-02-01 10:41:52 +0530

    variable demo 2
    Signed-off-by: BENSON <BENSON@LAPTOP-N29UTT3F>

--- java project/src/variabledemo2.java
+++ java project/src/variabledemo2.java
@@ -1,4 +1,24 @@
-
+/*
+ * OBJECTIVES
+ * 1.Valid identifiers
+ */
 public class variabledemo2 {
-
+ public static void main(String[] args) {
+	int d;//d is a valid name
+	int size;//size ia a valid name
+	
+	int size;//valid name-name can start with underscore
+	int $size;//valid name-name can start with $
+	int si3ze;//valid name-name can have nunbers in between
+	int si_2_$ze;//valid name-name can have _,$ and numbers anywhere
+	
+	//INVALID
+	//int 2size;//invalid-name cannot start with numbers
+	//int &size;//invalid-name cannot start with anyother special characters other than _ and &
+	//int si%ze;// invalid-name cannot have anyother special characters in between other than _ and $
+	//int class;//invalid-name cannot have keywords as identfiers
+	
+	//the same is applicable for all the data types
+	
+}
 }
